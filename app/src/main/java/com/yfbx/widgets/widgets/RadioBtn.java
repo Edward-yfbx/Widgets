d1e592ee330cdfeded6c10c61419873f02cdc5e8
Edward <yfbx0416@163.com>
2018-10-22 10:57:39 +0800

    update

---
+++ app/src/main/java/com/yfbx/widgets/widgets/RadioBtn.java
@@ -0,0 +1,31 @@
+package com.yfbx.widgets.widgets;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Author:Edward
+ * Date:2018/10/22
+ * Description:
+ */
+
+public class RadioBtn extends View {
+
+    public RadioBtn(Context context) {
+        super(context);
+    }
+
+    public RadioBtn(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RadioBtn(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public RadioBtn(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
