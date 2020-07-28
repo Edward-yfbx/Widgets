d36f09d89ae931ac3207eb384f49cfcd551a0d86
Edward <edward6934@dingtalk.com>
2020-07-28 15:12:40 +0800

    adapter binder

--- app/src/main/java/com/yfbx/widgets/activity/MainActivity.kt
+++ app/src/main/java/com/yfbx/widgets/activity/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.yfbx.widgets.activity
 
 import android.os.Bundle
+import android.view.Gravity
 import com.yfbx.widgets.R
+import com.yfbx.widgets.adapter.ViewHelper
 import com.yfbx.widgets.adapter.bind
 import com.yfbx.widgets.bean.Menu
+import com.yfbx.widgets.util.findColor
 import com.yfbx.widgets.util.startScheme
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.item_menu.*
@@ -17,35 +20,52 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val menus = arrayListOf(
+                Menu.ANIM,
+                Menu.DRAW,
+                Menu.RADIO,
+                Menu.ROLL,
+                Menu.TEXT,
+                Menu.TEST,
+                Menu.WEB
+        )
+
+        //单布局
         recycleView.bind(R.layout.item_menu, menus) { helper, item ->
             helper.btn.text = item.title
             helper.btn.setOnClickListener { startScheme(item.scheme) }
         }
 
+        //多布局
         recycleView.bind {
-            bind<String>(R.layout.item_menu_test, "Group-1") { helper, item ->
-                helper.textView.text = item
+            bind(R.layout.item_menu_test, "Group-1") { helper, item ->
+                groupBinder(helper, item)
             }
             bind<Menu>(R.layout.item_menu, menus) { helper, item ->
                 helper.btn.text = item.title
                 helper.btn.setOnClickListener { startScheme(item.scheme) }
             }
-            bind<String>(R.layout.item_menu_test, "Group-2") { helper, item ->
+            bind(R.layout.item_menu_test, "Group-2") { helper, item ->
+                groupBinder(helper, item)
+            }
+            bind(R.layout.item_menu_test, "test1", "test2", "test3") { helper, item ->
                 helper.textView.text = item
             }
-            bind<String>(R.layout.item_menu_test, listOf("test1", "test2", "test3")) { helper, item ->
-                helper.textView.text = item
+            bind(R.layout.item_menu_test, "Group-3") { helper, item ->
+                groupBinder(helper, item)
+            }
+            bind<Menu>(R.layout.item_menu, menus) { helper, item ->
+                helper.btn.text = item.title
+                helper.btn.setOnClickListener { startScheme(item.scheme) }
             }
         }
     }
 
-    private val menus = arrayListOf(
-            Menu.ANIM,
-            Menu.DRAW,
-            Menu.RADIO,
-            Menu.ROLL,
-            Menu.TEXT,
-            Menu.TEST,
-            Menu.WEB
-    )
+
+    private fun groupBinder(helper: ViewHelper, item: String) {
+        helper.textView.setBackgroundResource(R.color.background)
+        helper.textView.setTextColor(findColor(R.color.fontDark))
+        helper.textView.gravity = Gravity.START
+        helper.textView.text = item
+    }
 }
--- app/src/main/java/com/yfbx/widgets/adapter/AdapterExt.kt
+++ app/src/main/java/com/yfbx/widgets/adapter/AdapterExt.kt
@@ -4,7 +4,6 @@ import android.view.View
 import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
 import com.chad.library.adapter.base.BaseMultiItemQuickAdapter
-import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.chad.library.adapter.base.entity.MultiItemEntity
 import kotlinx.android.extensions.LayoutContainer
@@ -15,30 +14,14 @@ import kotlinx.android.extensions.LayoutContainer
  * Description:
  */
 
-fun <T> RecyclerView.bind(creator: SimpleCreator<T>.() -> Unit): BaseQuickAdapter<T, ViewHelper> {
-    val mAdapter = SimpleCreator<T>().apply(creator).create()
-    adapter = mAdapter
-    return mAdapter
-}
 
-fun <T> RecyclerView.bind(@LayoutRes layoutId: Int, data: List<T> = listOf(), onBind: (helper: ViewHelper, item: T) -> Unit): BaseQuickAdapter<T, ViewHelper> {
-    val mAdapter = SimpleCreator<T>().apply {
-        setDate(data)
-        bind(layoutId, onBind)
+fun <T> RecyclerView.bind(@LayoutRes layoutId: Int, data: List<T>, binder: (helper: ViewHelper, item: T) -> Unit): BaseMultiItemQuickAdapter<MultiEntity<*>, ViewHelper> {
+    val mAdapter = MultiCreator().apply {
+        bind(layoutId, data, binder)
     }.create()
     adapter = mAdapter
     return mAdapter
 }
-
-fun <T> RecyclerView.bind(view: View, data: List<T> = listOf(), onBind: (helper: ViewHelper, item: T) -> Unit): BaseQuickAdapter<T, ViewHelper> {
-    val mAdapter = SimpleCreator<T>().apply {
-        setDate(data)
-        bind(view, onBind)
-    }.create()
-    adapter = mAdapter
-    return mAdapter
-}
-
 
 fun RecyclerView.bind(creator: MultiCreator.() -> Unit): BaseMultiItemQuickAdapter<MultiEntity<*>, ViewHelper> {
     val mAdapter = MultiCreator().apply(creator).create()
@@ -46,67 +29,21 @@ fun RecyclerView.bind(creator: MultiCreator.() -> Unit): BaseMultiItemQuickAdapt
     return mAdapter
 }
 
-class SimpleCreator<T> {
-    @LayoutRes
-    private var layoutId: Int = 0
-    private var items: List<T> = listOf()
-    private var binder: ((helper: ViewHelper, item: T) -> Unit)? = null
-
-    fun setDate(data: List<T>) {
-        this.items = data
-    }
-
-    fun bind(@LayoutRes layoutId: Int = 0, binder: (helper: ViewHelper, item: T) -> Unit) {
-        this.binder = binder
-        this.layoutId = layoutId
-    }
-
-    fun bind(view: View, binder: (helper: ViewHelper, item: T) -> Unit) {
-        view.id = View.generateViewId()
-        this.binder = binder
-        this.layoutId = view.id
-    }
-
-    fun create() = object : BaseQuickAdapter<T, ViewHelper>(layoutId, items) {
-        override fun convert(helper: ViewHelper, item: T) {
-            binder?.invoke(helper, item)
-        }
-    }
-}
-
 
 @Suppress("UNCHECKED_CAST")
 class MultiCreator {
     private val items = mutableListOf<MultiEntity<*>>()
     private val layouts = hashMapOf<Int, Int>()
-    private val binders = hashMapOf<Int, (helper: ViewHelper, item: Any) -> Unit>()
 
-    fun <T> bind(@LayoutRes layoutId: Int, data: Any, binder: (helper: ViewHelper, item: T) -> Unit) {
-        items.add(MultiEntity(data, layoutId))
+    fun <T> bind(@LayoutRes layoutId: Int, vararg data: T, binder: (helper: ViewHelper, item: T) -> Unit) {
+        items.addAll(data.map { MultiEntity(it, layoutId, binder) })
         layouts[layoutId] = layoutId
-        binders[layoutId] = binder as ((helper: ViewHelper, item: Any) -> Unit)
     }
 
-    fun <T> bind(@LayoutRes layoutId: Int, data: List<Any>, binder: (helper: ViewHelper, item: T) -> Unit) {
-        items.addAll(data.map { MultiEntity(it, layoutId) })
+    fun <T> bind(@LayoutRes layoutId: Int, data: List<T>, binder: (helper: ViewHelper, item: T) -> Unit) {
+        items.addAll(data.map { MultiEntity(it, layoutId, binder) })
         layouts[layoutId] = layoutId
-        binders[layoutId] = binder as ((helper: ViewHelper, item: Any) -> Unit)
     }
-
-    fun <T> bind(view: View, data: Any, binder: (helper: ViewHelper, item: T) -> Unit) {
-        view.id = View.generateViewId()
-        items.add(MultiEntity(data, view.id))
-        layouts[view.id] = view.id
-        binders[view.id] = binder as ((helper: ViewHelper, item: Any) -> Unit)
-    }
-
-    fun <T> bind(view: View, data: List<Any>, binder: (helper: ViewHelper, item: T) -> Unit) {
-        view.id = View.generateViewId()
-        items.addAll(data.map { MultiEntity(it, view.id) })
-        layouts[view.id] = view.id
-        binders[view.id] = binder as ((helper: ViewHelper, item: Any) -> Unit)
-    }
-
 
     fun create() = object : BaseMultiItemQuickAdapter<MultiEntity<*>, ViewHelper>(items) {
         init {
@@ -116,18 +53,24 @@ class MultiCreator {
         }
 
         override fun convert(helper: ViewHelper, item: MultiEntity<*>?) {
-            item?.let {
-                binders[it.itemType]?.invoke(helper, item.item!!)
-            }
+            item?.onBind(helper)
         }
     }
 }
 
 
-class MultiEntity<T>(val item: T, private val type: Int) : MultiItemEntity {
+class MultiEntity<T>(
+        private val entity: T,
+        private val layoutId: Int,
+        private val binder: (helper: ViewHelper, item: T) -> Unit
+) : MultiItemEntity {
 
     override fun getItemType(): Int {
-        return type
+        return layoutId
+    }
+
+    fun onBind(helper: ViewHelper) {
+        binder.invoke(helper, entity)
     }
 }
 
